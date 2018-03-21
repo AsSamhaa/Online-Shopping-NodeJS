ac278967829b018f81aa81b9c9786eb040962752
HagarAbdelwahab <hagarabdelwahab@localhost.localdomain>
2018-03-21 19:12:21 +0200

    socialmedia without moongose

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
+var socialmedia = require('./routes/socialmedia');
 
 var fs = require("fs");
 //connect with database using mongoose
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/socialmedia',socialmedia);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
